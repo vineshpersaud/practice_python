2b308ba44ec68da378acc0e572620031fe14ad3c
github username <github email address>
2019-07-05 15:20:31 -0400

    first commit

---
+++ if_else.py
@@ -0,0 +1,7 @@
+language = 'Ruby'
+if language == 'Python':
+    print('language is Python')
+elif language == 'Ruby':
+    print('language is Ruby')
+else:
+    print('no match')
---
+++ loops.py
@@ -0,0 +1,4 @@
+students = ["Kelly","Bob","Mike"]
+
+for student in students:
+    print('Welcome to the first day of class {}.'.format(student))
