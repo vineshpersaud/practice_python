ec4a31a3921f0eca6378460f05a5573784bc3bcd
github username <github email address>
2019-07-13 15:20:54 -0400

    string operations

---
+++ function_input.py
@@ -0,0 +1,5 @@
+def hello():
+    name = raw_input("Enter your name : ")
+    print('Hello {}'.format(name.capitalize()))
+
+hello()
---
+++ string_operation.py
@@ -0,0 +1,9 @@
+def string_length():
+    string = raw_input('Please enter String: ')
+    print(len(string))
+
+
+def string_length_method(string):
+    print(len(string))
+
+string_length_method("hello")
